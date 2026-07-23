274e7c9f97ec259a93831691707d8f2df6647c97
Ramenzayebal <yojkin.nikolai@yandex.ru>
2026-07-23 15:53:04 +0300

    Add files via upload

---
+++ TelephoneCat/TelephoneCat/common.h
@@ -0,0 +1,15 @@
+#pragma once // Гарантирует, что файл подключится только один раз
+#define _CRT_SECURE_NO_WARNINGS
+// Подключаем стандартную библиотеку ввода-вывода для всех файлов
+#include <stdio.h>
+#include <stdlib.h> 
+#include <string.h>
+#include <conio.h>
+#include <stdbool.h>
+
+// Функции для работы с файлом
+#include "phonebook.h"
+#include "filefunc.h"
+// Общие макросы проекта
+#define SUCCESS 0
+#define ERROR -1
