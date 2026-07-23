6fb412c8cfbc8efabeebb8aa8888669c5a7ebdc5
Ramenzayebal <yojkin.nikolai@yandex.ru>
2026-07-23 20:13:54 +0300

    Delete filefunc.h

--- filefunc.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "common.h"
-
-int loadFILE(const char*, Contact*, int);
-int saveFILE(const char* , Contact* , int);
-bool FILEexist(const char* );
-void check_string(char* str);
