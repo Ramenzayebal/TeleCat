cb2ffa96cd99c55d09d2e0f5c8effc7a76d20346
Ramenzayebal <yojkin.nikolai@yandex.ru>
2026-07-24 15:20:21 +0300

    Delete TelephoneCat/TelephoneCat directory

--- TelephoneCat/TelephoneCat/filefunc.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "common.h"
-#include "phonebook.h" // Подключаем именно здесь, чтобы тип Phonebook был известен!
-
-int loadFILE(const char* filename, Phonebook* pb);
-int saveFILE(const char* filename, Phonebook* pb);
-bool FILEexist(const char* filename);
-void correct_string(char* str);
